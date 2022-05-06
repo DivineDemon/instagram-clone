6867a42d318878bb04a13b11d9e8deb54b3c5213
DivineDemon <supame123@gmail.com>
2022-05-07 01:58:12 +0400

    :tada:

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,14 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Instagram Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Instagram Clone</h1>
+    </div>
+  );
 }
