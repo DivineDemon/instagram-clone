3b3318ce06b00907a3c89b4675d6d368b8a2152d
DivineDemon <supame123@gmail.com>
2022-05-07 17:27:01 +0400

    Header Nav Done! ✨

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Header from "./../components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
         <title>Instagram Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Instagram Clone</h1>
+      <Header />
     </div>
   );
 }
