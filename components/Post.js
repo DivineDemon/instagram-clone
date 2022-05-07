40ca02ea99872eae23b904a0fdcc30b1cb6cc985
DivineDemon <supame123@gmail.com>
2022-05-07 21:09:13 +0400

    Posts Done! ✨

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,62 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 
-function Post() {
-  return <div>Post</div>;
+function Post({ id, username, userImg, img, caption }) {
+  return (
+    <div className="bg-white my-7 border rounded-sm">
+      {/* Header */}
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          alt="post-user"
+          className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+        />
+        <p className="flex-1 font-bold">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+
+      {/* img */}
+      <img src={img} alt="post" className="object-cover w-full" />
+
+      {/* Buttons */}
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+          <PaperAirplaneIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
+
+      {/* Caption */}
+      <p className="p-5 truncate">
+        <span className="font-bold mr-1">{username} </span>
+        {caption}
+      </p>
+
+      {/* Comments */}
+
+      {/* Comment Input */}
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="h-7" />
+        <input
+          type="text"
+          className="border-none flex-1 focus:ring-0"
+          placeholder="Add a Comment..."
+        />
+        <button className="font-semibold text-blue-400">Post</button>
+      </form>
+    </div>
+  );
 }
 
 export default Post;
--- components/Posts.js
+++ components/Posts.js
@@ -1,7 +1,40 @@
 import React from "react";
+import Post from "./Post";
+
+const posts = [
+  {
+    id: "1",
+    username: "mushood_7",
+    userImg:
+      "https://lh3.googleusercontent.com/ogw/ADea4I51tAioLLXaORYgQAYasUYcPAt2i1OJcluPIn-SwdI=s32-c-mo",
+    img: "https://i.imgur.com/c6dqdeQ.png",
+    caption: "Death!",
+  },
+  {
+    id: "2",
+    username: "mushood_7",
+    userImg:
+      "https://lh3.googleusercontent.com/ogw/ADea4I51tAioLLXaORYgQAYasUYcPAt2i1OJcluPIn-SwdI=s32-c-mo",
+    img: "https://i.imgur.com/e5FYFXN.png",
+    caption: "The Emperor!",
+  },
+];
 
 function Posts() {
-  return <div>Posts</div>;
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.username}
+          userImg={post.userImg}
+          img={post.img}
+          caption={post.caption}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Posts;
