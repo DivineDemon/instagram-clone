332c61265cf49a7ad86504f9eb9e9ee89fd9e772
DivineDemon <supame123@gmail.com>
2022-05-07 22:29:33 +0400

    GUI Completed, Starting User Auth! ✨

--- components/Stories.js
+++ components/Stories.js
@@ -3,19 +3,19 @@ import { faker } from "@faker-js/faker";
 import Story from "./Story";
 
 function Stories() {
-  const [suggesstions, setSuggesstions] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
 
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
       ...faker.helpers.contextualCard(),
       id: i,
     }));
-    setSuggesstions(suggestions);
+    setSuggestions(suggestions);
   }, []);
 
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
-      {suggesstions.map((profile) => (
+      {suggestions.map((profile) => (
         <Story
           key={profile.id}
           img={profile.avatar}
