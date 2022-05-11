8461fcd60e5133998ce515d9a9305b0aeb7fe4fe
DivineDemon <supame123@gmail.com>
2022-05-12 02:38:44 +0400

    :hammer:

--- components/Posts.js
+++ components/Posts.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import { onSnapshot, collection, query, orderBy } from "@firebase/firestore";
 import { db } from "../firebase";
